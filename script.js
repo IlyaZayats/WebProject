133a4edc31bec7620e95acffe9dc0c18f4849f16
IlyaZayats <zayatsman@gmail.com>
2021-11-18 20:32:37 +0300

    Almost completed header

--- script.js
+++ script.js
@@ -2,15 +2,26 @@
 function moveNavbar(){
     let windowInnerWidth = window.innerWidth;
     let navbar = $("#navbar");
-    if(windowInnerWidth <= 576) {
+    if(windowInnerWidth <= 768) {
         navbar.addClass("fixed-bottom");
         navbar.addClass("bg-dark");
-
     }
-    if(windowInnerWidth >= 576){
+    if(windowInnerWidth >= 768){
         navbar.removeClass("fixed-bottom");
         navbar.removeClass("bg-dark");
     }
+}
+
+function showDropdown(navbarDropdown, list){
+    $(navbarDropdown).addClass("show");
+    $(navbarDropdown).prop("aria-expanded", true);
+    $(list).addClass("show");
+}
+
+function hideDropdown(navbarDropdown, list){
+    $(navbarDropdown).removeClass("show");
+    $(navbarDropdown).prop("aria-expanded", false);
+    $(list).removeClass("show");
 }
 
 
