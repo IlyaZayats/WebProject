26fd2348330c7614b3e272ef33c5cd418674f50c
Melikov Roman <melikvov@mail.ru>
2021-11-20 20:18:03 +0300

    Исправка и оптимизация кода аккордеона, чуть улучшил стиль аккордеона. Добил базу обзоров смищными копипастами. Есть проблемы с псевдослайдами, смотри issue

--- accordion.js
+++ accordion.js
@@ -1,18 +1,24 @@
-// Функция внутри $( document ).ready() срабатывает после загрузки DOM.
 $(document).ready(function() {
   /*
    $(".accordion-group:first").addClass("toggle");
  $(".accordion-header:first").addClass("active"); первый элемент включен?
  $(".accordion-content:not(:first)").hide();
   */
+ let oldelem=null;
  $(".accordion-content").hide();
  $(".accordion-header").click(function () {
-   $(this).next().slideToggle("slow");
-   $(".accordion-content").not($(this).next()).slideUp("slow");
+   if($(this).next().is(":animated")) return;
+   $(this).next().slideToggle();
    $(this).toggleClass("active");
-   $(".accordion-header").not($(this)).removeClass("active");
    $(this).parent().toggleClass("toggle");
-   $(".accordion-group").not($(this).parent()).removeClass("toggle");
+   if(oldelem===this) return;
+   //$(".accordion-content").not($(this).next()).slideUp();
+   $(oldelem).next().slideUp();
+   //$(".accordion-header").not($(this)).removeClass("active");
+   $(oldelem).removeClass("active");
+   $(oldelem).parent().removeClass("toggle");
+   oldelem=this;
+   //$(".accordion-group").not($(this).parent()).removeClass("toggle");
  });
 
     
